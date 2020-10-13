ce78f340554982aa824faadaab9d55792eda098f
yyy110011 <yyy110011@livemail.tw>
2020-10-13 14:29:56 +0800

    train 3D without lighting

--- train3D.py
+++ train3D.py
@@ -10,6 +10,7 @@ import time
 import random
 import argparse
 from eval.evaluator import *
+from eval.froc import calculate_FROC
 from utils.tools import *
 #from tensorboardX import SummaryWriter
 from torch.utils.tensorboard import SummaryWriter
@@ -20,6 +21,9 @@ from utils.log import Logger
 from eval_coco import *
 from eval.cocoapi_evaluator import COCOAPIEvaluator
 
+from databuilder.abus import ABUSDetectionDataset
+from databuilder.yolo4dataset import YOLO4_3DDataset
+from tqdm import tqdm
 
 def detection_collate(batch):
     targets = []
@@ -31,7 +35,8 @@ def detection_collate(batch):
 
 
 class Trainer(object):
-    def __init__(self,  weight_path, resume, gpu_id, accumulate, fp_16):
+    def __init__(self, weight_path, checkpoint_save_dir, resume, gpu_id, accumulate, fp_16):
+        self.data_root = 'datasets/abus'
         init_seeds(0)
         self.fp_16 = fp_16
         self.device = gpu.select_device(gpu_id)
@@ -39,39 +44,52 @@ class Trainer(object):
         self.best_mAP = 0.
         self.accumulate = accumulate
         self.weight_path = weight_path
+        self.checkpoint_save_dir = checkpoint_save_dir
         self.multi_scale_train = cfg.TRAIN["MULTI_SCALE_TRAIN"]
         if self.multi_scale_train:print('Using multi scales training')
         else:print('train img size is {}'.format(cfg.TRAIN["TRAIN_IMG_SIZE"]))
-        self.train_dataset = data.Build_Dataset(anno_file_type="train", img_size=cfg.TRAIN["TRAIN_IMG_SIZE"])
+
+        train_dataset = ABUSDetectionDataset(augmentation=True, crx_fold_num= 0, crx_partition= 'train', crx_valid=True, include_fp=False, root=self.data_root)
+        self.train_dataset = YOLO4_3DDataset(train_dataset, classes=[0, 1], img_size=cfg.TRAIN["TRAIN_IMG_SIZE"])
+        #self.train_dataset = data.Build_Dataset(anno_file_type="train", img_size=cfg.TRAIN["TRAIN_IMG_SIZE"])
+
         self.epochs = cfg.TRAIN["YOLO_EPOCHS"] if cfg.MODEL_TYPE["TYPE"] == 'YOLOv4' else cfg.TRAIN["Mobilenet_YOLO_EPOCHS"]
         self.train_dataloader = DataLoader(self.train_dataset,
                                            batch_size=cfg.TRAIN["BATCH_SIZE"],
-                                           num_workers=0, #cfg.TRAIN["NUMBER_WORKERS"],
+                                           num_workers=cfg.TRAIN["NUMBER_WORKERS"],
                                            shuffle=True, pin_memory=True
                                            )
 
-        self.yolov4 = Build_Model(weight_path=weight_path, resume=resume).to(self.device)
+        test_dataset = ABUSDetectionDataset(augmentation=False, crx_fold_num= 0, crx_partition= 'train', crx_valid=True, include_fp=False, root=self.data_root)
+        self.test_dataset = YOLO4_3DDataset(test_dataset, classes=[0, 1], img_size=cfg.VAL["TEST_IMG_SIZE"])
+        self.test_dataloader = DataLoader(self.test_dataset,
+                                            batch_size=cfg.VAL["BATCH_SIZE"],
+                                            num_workers=cfg.VAL["NUMBER_WORKERS"],
+                                            shuffle=False, pin_memory=True
+                                            )
 
-        self.optimizer = optim.SGD(self.yolov4.parameters(), lr=cfg.TRAIN["LR_INIT"],
+        self.model = Build_Model(weight_path=weight_path, resume=resume, dims=3).to(self.device)
+
+        self.optimizer = optim.SGD(self.model.parameters(), lr=cfg.TRAIN["LR_INIT"],
                                    momentum=cfg.TRAIN["MOMENTUM"], weight_decay=cfg.TRAIN["WEIGHT_DECAY"])
 
         self.criterion = YoloV4Loss(anchors=cfg.MODEL["ANCHORS"], strides=cfg.MODEL["STRIDES"],
-                                    iou_threshold_loss=cfg.TRAIN["IOU_THRESHOLD_LOSS"])
+                                    iou_threshold_loss=cfg.TRAIN["IOU_THRESHOLD_LOSS"], dims=3)
 
         self.scheduler = cosine_lr_scheduler.CosineDecayLR(self.optimizer,
-                                                          T_max=self.epochs,
+                                                          T_max=self.epochs*len(self.train_dataloader),
                                                           lr_init=cfg.TRAIN["LR_INIT"],
                                                           lr_min=cfg.TRAIN["LR_END"],
-                                                          warmup=cfg.TRAIN["WARMUP_EPOCHS"])
+                                                          warmup=cfg.TRAIN["WARMUP_EPOCHS"]*len(self.train_dataloader))
         if resume: self.__load_resume_weights(weight_path)
-
+        logger.info(self.model)
     def __load_resume_weights(self, weight_path):
 
-        last_weight = os.path.join(os.path.split(weight_path)[0], "last.pt")
+        last_weight = os.path.join(weight_path)
         chkpt = torch.load(last_weight, map_location=self.device)
-        self.yolov4.load_state_dict(chkpt['model'])
-
-        self.start_epoch = chkpt['epoch'] + 1
+        self.model.load_state_dict(chkpt['model'])
+        if chkpt['epoch'] is not None:
+            self.start_epoch = chkpt['epoch'] + 1
         if chkpt['optimizer'] is not None:
             self.optimizer.load_state_dict(chkpt['optimizer'])
             self.best_mAP = chkpt['best_mAP']
@@ -80,19 +98,17 @@ class Trainer(object):
     def __save_model_weights(self, epoch, mAP):
         if mAP > self.best_mAP:
             self.best_mAP = mAP
-        best_weight = os.path.join(os.path.split(self.weight_path)[0], "best.pt")
-        last_weight = os.path.join(os.path.split(self.weight_path)[0], "last.pt")
+
         chkpt = {'epoch': epoch,
                  'best_mAP': self.best_mAP,
-                 'model': self.yolov4.state_dict(),
+                 'model': self.model.state_dict(),
                  'optimizer': self.optimizer.state_dict()}
-        torch.save(chkpt, last_weight)
 
-        if self.best_mAP == mAP:
-            torch.save(chkpt['model'], best_weight)
-
-        if epoch > 0 and epoch % 10 == 0:
-            torch.save(chkpt, os.path.join(os.path.split(self.weight_path)[0], 'backup_epoch%g.pt'%epoch))
+        if epoch > 0 and epoch % 20 == 0:
+            torch.save(chkpt, os.path.join(self.checkpoint_save_dir, 'backup_epoch%g.pt'%epoch))
+        if epoch==0 or (self.best_mAP == mAP and mAP>0):
+            torch.save(chkpt, os.path.join(self.checkpoint_save_dir, "best.pt"))
+        #torch.save(chkpt, os.path.join(self.checkpoint_save_dir, "last.pt"))
         del chkpt
 
 
@@ -100,18 +116,17 @@ class Trainer(object):
     def train(self):
         global writer
         logger.info("Training start,img size is: {},batchsize is: {:d},work number is {:d}".format(cfg.TRAIN["TRAIN_IMG_SIZE"],cfg.TRAIN["BATCH_SIZE"],cfg.TRAIN["NUMBER_WORKERS"]))
-        logger.info(self.yolov4)
         logger.info("Train datasets number is : {}".format(len(self.train_dataset)))
 
-        if self.fp_16: self.yolov4, self.optimizer = amp.initialize(self.yolov4, self.optimizer, opt_level='O1', verbosity=0)
+        if self.fp_16: self.model, self.optimizer = amp.initialize(self.model, self.optimizer, opt_level='O1', verbosity=0)
         logger.info("        =======  start  training   ======     ")
         for epoch in range(self.start_epoch, self.epochs):
             start = time.time()
-            self.yolov4.train()
+            self.model.train()
 
             mloss = torch.zeros(4)
             logger.info("===Epoch:[{}/{}]===".format(epoch, self.epochs))
-            for i, (imgs, label_sbbox, label_mbbox, label_lbbox, sbboxes, mbboxes, lbboxes)  in enumerate(self.train_dataloader):
+            for i, (imgs, label_sbbox, label_mbbox, label_lbbox, sbboxes, mbboxes, lbboxes, img_names)  in tqdm(enumerate(self.train_dataloader)):
                 self.scheduler.step(len(self.train_dataloader)*epoch + i)
 
                 imgs = imgs.to(self.device)
@@ -122,8 +137,7 @@ class Trainer(object):
                 mbboxes = mbboxes.to(self.device)
                 lbboxes = lbboxes.to(self.device)
 
-                p, p_d = self.yolov4(imgs)
-
+                p, p_d = self.model(imgs)
                 loss, loss_ciou, loss_conf, loss_cls = self.criterion(p, p_d, label_sbbox, label_mbbox,
                                                   label_lbbox, sbboxes, mbboxes, lbboxes)
 
@@ -144,8 +158,8 @@ class Trainer(object):
                 # Print batch results
                 if i % 10 == 0:
 
-                    logger.info("  === Epoch:[{:3}/{}],step:[{:3}/{}],img_size:[{:3}],total_loss:{:.4f}|loss_ciou:{:.4f}|loss_conf:{:.4f}|loss_cls:{:.4f}|lr:{:.4f}".format(
-                        epoch, self.epochs,i, len(self.train_dataloader) - 1, self.train_dataset.img_size,mloss[3], mloss[0], mloss[1],mloss[2],self.optimizer.param_groups[0]['lr']
+                    logger.info("  === Epoch:[{:3}/{}],step:[{:3}/{}],img_size:[{}],total_loss:{:.4f}|loss_ciou:{:.4f}|loss_conf:{:.4f}|loss_cls:{:.4f}|lr:{:.4f}".format(
+                        epoch, self.epochs, i, len(self.train_dataloader) - 1, self.train_dataset.img_size,mloss[3], mloss[0], mloss[1],mloss[2],self.optimizer.param_groups[0]['lr']
                     ))
                     writer.add_scalar('loss_ciou', mloss[0],
                                       len(self.train_dataloader) * epoch + i)
@@ -155,59 +169,79 @@ class Trainer(object):
                                       len(self.train_dataloader) * epoch + i)
                     writer.add_scalar('train_loss', mloss[3],
                                       len(self.train_dataloader) * epoch + i)
+                    writer.add_scalar('train_lr', self.optimizer.param_groups[0]["lr"],
+                                      len(self.train_dataloader) * epoch + i)
                 # multi-sclae training (320-608 pixels) every 10 batches
                 if self.multi_scale_train and (i+1) % 10 == 0:
                     self.train_dataset.img_size = random.choice(range(10, 20)) * 32
 
 
-            if cfg.TRAIN["DATA_TYPE"] == 'VOC' or cfg.TRAIN["DATA_TYPE"] == 'Customer':
-                mAP = 0.
-                if epoch >= 0:
-                    logger.info("===== Validate =====".format(epoch, self.epochs))
-                    with torch.no_grad():
-                        APs, inference_time = Evaluator(self.yolov4, showatt=False).APs_voc()
-                        for i in APs:
-                            logger.info("{} --> mAP : {}".format(i, APs[i]))
-                            mAP += APs[i]
-                        mAP = mAP / self.train_dataset.num_classes
-                        logger.info("mAP : {}".format(mAP))
-                        logger.info("inference time: {:.2f} ms".format(inference_time))
-                        writer.add_scalar('mAP', mAP, epoch)
-                        self.__save_model_weights(epoch, mAP)
-                        logger.info('save weights done')
-                    logger.info("  ===test mAP:{:.3f}".format(mAP))
-            elif epoch >= 0 and cfg.TRAIN["DATA_TYPE"] == 'COCO':
-                evaluator = COCOAPIEvaluator(model_type='YOLOv4',
-                                             data_dir=cfg.DATA_PATH,
-                                             img_size=cfg.VAL["TEST_IMG_SIZE"],
-                                             confthre=0.08,
-                                             nmsthre=cfg.VAL["NMS_THRESH"])
-                ap50_95, ap50 = evaluator.evaluate(self.yolov4)
-                logger.info('ap50_95:{}|ap50:{}'.format(ap50_95, ap50))
-                writer.add_scalar('val/COCOAP50', ap50, epoch)
-                writer.add_scalar('val/COCOAP50_95', ap50_95, epoch)
-                self.__save_model_weights(epoch, ap50)
-                print('save weights done')
+            if cfg.TRAIN["DATA_TYPE"] == 'VOC' or cfg.TRAIN["DATA_TYPE"] == 'ABUS':
+                area = self.evaluate()[0]
+                logger.info("===== Validate =====".format(epoch, self.epochs))
+                writer.add_scalar('AUC', area, epoch)
+                self.__save_model_weights(epoch, area)
+                logger.info('save weights done')
+                logger.info("  ===test AUC:{:.3f}".format(area))
+
             end = time.time()
             logger.info("  ===cost time:{:.4f}s".format(end - start))
         logger.info("=====Training Finished.   best_test_mAP:{:.3f}%====".format(self.best_mAP))
 
+    def evaluate(self):
+        logger.info("Evaluate start,img size is: {},batchsize is: {:d},work number is {:d}".format(cfg.VAL["TEST_IMG_SIZE"], cfg.VAL["BATCH_SIZE"], cfg.VAL["NUMBER_WORKERS"]))
+        #logger.info(self.model)
+        logger.info("Test datasets number is : {}".format(len(self.test_dataloader)))
 
+        if self.fp_16: self.model, self.optimizer = amp.initialize(self.model, self.optimizer, opt_level='O1', verbosity=0)
+        logger.info("        =======  start  evaluate   ======     ")
+        start = time.time()
+        self.model.eval()
+        mloss = torch.zeros(4)
+        pred_result_path=os.path.join(self.checkpoint_save_dir, 'evaluate')
+        self.evaluator = Evaluator(self.model, showatt=False, pred_result_path=pred_result_path, box_top_k=50)
+        self.evaluator.clear_predict_file()
+        TOP_K = 50
+        with torch.no_grad():
+            for i, (imgs, label_sbbox, label_mbbox, label_lbbox, sbboxes, mbboxes, lbboxes, img_names)  in tqdm(enumerate(self.test_dataloader)):
+                imgs = imgs.to(self.device)
+                for img, img_name in zip(imgs, img_names):
+                    bboxes_prd = self.evaluator.get_bbox(img, multi_test=False, flip_test=False)
+                    self.evaluator.store_bbox(img_name, bboxes_prd)
+            npy_dir = pred_result_path
+            npy_format = npy_dir + '/{}_0.npy'
+            area, plt = calculate_FROC(self.data_root, npy_dir, npy_format, size_threshold=20)
+        end = time.time()
+        logger.info("  ===cost time:{:.4f}s".format(end - start))
+        return area, plt
 if __name__ == "__main__":
     global logger, writer
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weight_path', type=str, default='weight/mobilenetv3.pth', help='weight file path')#weight/darknet53_448.weights
+    parser.add_argument('--weight_path', type=str, default=None, help='weight file path')#weight/darknet53_448.weights
     parser.add_argument('--resume', action='store_true',default=False,  help='resume training flag')
     parser.add_argument('--gpu_id', type=int, default=-1, help='whither use GPU(eg:0,1,2,3,4,5,6,7,8) or CPU(-1)')
     parser.add_argument('--log_path', type=str, default='log/', help='log path')
-    parser.add_argument('--accumulate', type=int, default=2, help='batches to accumulate before optimizing')
+    parser.add_argument('--accumulate', type=int, default=1, help='batches to accumulate before optimizing')
     parser.add_argument('--fp_16', type=bool, default=False, help='whither to use fp16 precision')
+    parser.add_argument('--exp_name', type=str, default='debug', help='log experiment name')
     opt = parser.parse_args()
-    writer = SummaryWriter(log_dir=opt.log_path + '/event')
+    writer = SummaryWriter(log_dir=opt.log_path + '/' + opt.exp_name)
     logger = Logger(log_file_name=opt.log_path + '/log.txt', log_level=logging.DEBUG, logger_name='YOLOv4').get_log()
 
-    Trainer(weight_path=opt.weight_path,
-            resume=opt.resume,
+    checkpoint_save_dir = 'checkpoint/' + opt.exp_name
+    if not os.path.exists(checkpoint_save_dir):
+        os.mkdir(checkpoint_save_dir)
+
+    weight_path = opt.weight_path
+    resume = opt.resume
+
+    #resume = True
+    #weight_path = 'checkpoint/YOLO_ABUS_d30_Stem16_lr25/backup_epoch140.pt'
+    trainer = Trainer(weight_path=weight_path,
+            checkpoint_save_dir=checkpoint_save_dir,
+            resume=resume,
             gpu_id=opt.gpu_id,
             accumulate=opt.accumulate,
-            fp_16=opt.fp_16).train()
+            fp_16=opt.fp_16)
+
+    trainer.train()
