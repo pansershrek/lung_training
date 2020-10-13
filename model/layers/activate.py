31eb4242e64e79203a1f415c4a3e987f5a25f45a
yyy110011 <yyy110011@livemail.tw>
2020-10-13 14:25:40 +0800

    logger and MishCuda

--- model/layers/activate.py
+++ model/layers/activate.py
@@ -3,14 +3,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class Mish(nn.Module):
-    def __init__(self):
-        super(Mish).__init__()
-
-    def forward(self, x):
-        x = x * (torch.tanh(F.softplus(x)))
-        return x
-
 
 class Swish(nn.Module):
     def __init__(self):
--- utils/log.py
+++ utils/log.py
@@ -2,21 +2,22 @@ import logging
 
 
 class Logger(object):
-    def __init__(self,log_file_name,log_level,logger_name):
+    def __init__(self, log_level, logger_name, log_file_name=None):
         # firstly, create a logger
         self.__logger = logging.getLogger(logger_name)
         self.__logger.setLevel(log_level)
         # secondly, create a handler
-        file_handler = logging.FileHandler(log_file_name)
+        file_handler=None
+        if log_file_name: file_handler = logging.FileHandler(log_file_name)
         console_handler = logging.StreamHandler()
         # thirdly, define the output form of handler
         formatter = logging.Formatter(
             '[%(asctime)s]-[%(filename)s line:%(lineno)d]:%(message)s '
         )
-        file_handler.setFormatter(formatter)
+        if file_handler:file_handler.setFormatter(formatter)
         console_handler.setFormatter(formatter)
         # finally, add the Hander to logger
-        self.__logger.addHandler(file_handler)
+        if file_handler:self.__logger.addHandler(file_handler)
         self.__logger.addHandler(console_handler)
 
     def get_log(self):
