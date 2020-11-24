6ade70093455c53179e6f2949d1bdbfaf42ccad4
bruce <brucetusec@gmail.com>
2020-11-24 20:23:49 +0800

    testing evaluation

--- draw_froc.py
+++ draw_froc.py
@@ -36,23 +36,67 @@ if __name__ == "__main__":
     parser.add_argument('--fp_16', type=bool, default=False, help='whither to use fp16 precision')
     parser.add_argument('--exp_name', type=str, default='debug', help='log experiment name')
     opt = parser.parse_args()
-    logger = Logger(log_file_name=opt.log_path + '/log.txt', log_level=logging.DEBUG, logger_name='YOLOv4').get_log()
-    checkpoint_save_dir = 'testing/' + opt.exp_name
-    if not os.path.exists('testing'):
-        os.mkdir('testing')
-    if not os.path.exists(checkpoint_save_dir):
-        os.mkdir(checkpoint_save_dir)
 
-    weight_path = opt.weight_path
-    trainer = Trainer(weight_path=weight_path,
-            checkpoint_save_dir=checkpoint_save_dir,
-            resume=False,
-            gpu_id=opt.gpu_id,
-            accumulate=1,
-            fp_16=opt.fp_16,
-            writer=None,
-            logger=logger)
 
-    trainer.evaluate()
+    for exp_name, fold_num in [
+        #('Fd0_BS2_Stem4_8_128_r2', 0),
+        #('Fd1_BS2_Stem4_8_128_r2', 1),
+        #('Fd2_BS2_Stem4_8_128_r2', 2),
+        #('Fd3_BS2_Stem4_8_128_r2', 3),
+        ('Fd4_BS2_Stem4_8_128_r2', 4),
+
+        #('Fd0_BS1_Stem4_16_256_r2', 0),
+        #('Fd1_BS1_Stem4_16_256_r2', 1),
+        #('Fd2_BS1_Stem4_16_256_r2', 2),
+        #('Fd3_BS1_Stem4_16_256_r2', 3),
+        #('Fd4_BS1_Stem4_16_256_r2', 4),
+        ]:
+        for testing_mode in [0, 1]:
+            if testing_mode==0 and exp_name=='Fd0_BS2_Stem4_8_128_r2':
+                continue
+
+            opt.exp_name = exp_name
+            logger = Logger(log_file_name=opt.log_path + '/log.txt', log_level=logging.DEBUG, logger_name='YOLOv4').get_log()
+            checkpoint_root = '/home/lab402/p08922003/YOLOv4-pytorch/checkpoint/'
+            checkpoint_folder = '{}{}'.format(checkpoint_root, exp_name)
+            checkpoints = os.listdir(checkpoint_folder)
+
+            phase = 'VAL'
+            if testing_mode: phase = 'TEST'
+            writer = SummaryWriter(log_dir=opt.log_path + '/{}_'.format(phase) + opt.exp_name)
+
+            for epoch in range(20):
+                weight_path = '{}/backup_epoch{}.pt'.format(checkpoint_folder, str(epoch))
+                if os.path.exists(weight_path):
+                    opt.weight_path = weight_path
+                    exp_name_folder = opt.exp_name
+                    if testing_mode==1:
+                        exp_name_folder = opt.exp_name + '_testing'
+                    checkpoint_save_dir = 'preidction/{}/{}'.format(exp_name_folder, str(epoch))
+                    if not os.path.exists('preidction'):
+                        os.mkdir('preidction')
+
+                    if not os.path.exists('preidction/{}'.format(exp_name_folder)):
+                        os.mkdir('preidction/{}'.format(exp_name_folder))
+
+                    if not os.path.exists(checkpoint_save_dir):
+                        os.mkdir(checkpoint_save_dir)
+
+                    weight_path = opt.weight_path
+                    weight_path = 'checkpoint/96_B4_F1/backup_epoch150.pt'
+                    trainer = Trainer(testing_mode=testing_mode,
+                            weight_path=weight_path,
+                            checkpoint_save_dir=checkpoint_save_dir,
+                            resume=False,
+                            gpu_id=opt.gpu_id,
+                            accumulate=1,
+                            fp_16=opt.fp_16,
+                            writer=None,
+                            logger=logger,
+                            crx_fold_num=fold_num)
+
+                    area_small, area_big, plt = trainer.evaluate()
+                    writer.add_scalar('AUC_10mm', area_small, epoch)
+                    writer.add_scalar('AUC_15mm', area_big, epoch)
 
 
