cc6a417b69e93adc24fb793d445dddfae8a7881a
GlebSkiba <gleb.skiba@philips.com>
2022-10-06 13:31:37 +0400

    update

--- pancreas_trainer.py
+++ pancreas_trainer.py
@@ -328,7 +328,7 @@ class Trainer:
                     data["images"]
                 )
                 image = utils.resize_without_pad(
-                    data["images"][0].view(self.image_size),
+                    data["images"][0].view(self.image_size).numpy(),
                     [int(x) for x in data["original_size"][0]],
                     "trilinear",
                     align_corners=False
